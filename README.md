1836bdad907f7f8ed1ec451b0f22e1ce3d07e74b
K1ng-T0ast <kylejwhite@proton.me>
2023-04-18 16:33:49 -0700

    Server testing complete, resolved lat and lon floating point issue in search parameter matched with json data

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
-# city-explorer-api
+# Project Name
+
+**Author**: Your Name Goes Here
+**Version**: 1.0.0 (increment the patch/fix version number if you make more commits past your first submission)
+
+## Overview
+<!-- Provide a high level overview of what this application is and why you are building it, beyond the fact that it's an assignment for this class. (i.e. What's your problem domain?) -->
+
+## Getting Started
+<!-- What are the steps that a user must take in order to build this app on their own machine and get it running? -->
+
+## Architecture
+<!-- Provide a detailed description of the application design. What technologies (languages, libraries, etc) you're using, and any other relevant design information. -->
+
+## Change Log
+<!-- Use this area to document the iterative changes made to your application as each feature is successfully implemented. Use time stamps. Here's an example:
+
+01-01-2001 4:59pm - Application now has a fully-functional express server, with a GET route for the location resource. -->
+
+## Credit and Collaborations
+<!-- Give credit (and a link) to other people or resources that helped you build this application. -->
+
+## Time Estimates
+
+### (Version 1.0.0)
+
+Name of feature: Setup Server Repository
+
+Estimate of time needed to complete: 15min
+
+Start time: 3:14
+
+Finish time: 3:34
+
+Actual time needed to complete: 14min
+
+###########################################################################
+
+Name of feature: Locations
+
+Estimate of time needed to complete: 35min
+
+Start time: 3:44
+
+Finish time: 4:16
+
+Actual time needed to complete: 32min
+
+###########################################################################
+
+Name of feature: Map
+
+Estimate of time needed to complete: 40min
+
+Start time: 4:18
+
+Finish time: 5:19
+
+Actual time needed to complete: 61min
+
+###########################################################################
+
+Name of feature: Error Handling
+
+Estimate of time needed to complete: 15min
+
+Start time: 5:20
+
+Finish time: 5:42
+
+Actual time needed to complete: 22min
+
+###########################################################################
---
+++ server.js
@@ -0,0 +1,88 @@
+'use strict';
+
+// **** REQUIRE **** like import but for the backend
+
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+let weatherData = require('./data/weather.json');
+
+// App now === server - Need to call express to create the server
+const app = express();
+
+// **** MIDDLEWARE **** Allow anyone to hit our server
+app.use(cors());
+
+const PORT = process.env.PORT || 3002;
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+// **** ENDPOINTS ****
+// **** 1st arg - endpoint URL, 2nd arg - callback which will execute when endpoint is hit
+// **** 2 parameters - request, response
+app.get('/', (req, res) => {
+    res.status(200).send('Welcome to my server!');
+});
+
+// app.get('/hello', (req, res) => {
+//     let firstName = req.query.userfirstName;
+//     let lastName = req.query.userlastName;
+
+//     console.log(req.query);
+
+//     res.status(200).send(`Hello ${firstName} ${lastName}, welcome to my server!`);
+// });
+
+// **** Weather goes here ****
+
+app.get('/weather', (req, res, next) => {
+    try {
+        let lat = parseFloat(req.query.lat);
+        let lon = parseFloat(req.query.lon);
+        let searchQuery = req.query.searchQuery;
+
+        console.log('lat:', lat, 'lon:', lon, 'searchQuery:', searchQuery);
+
+        let foundWeather = weatherData.find(city => city.city_name.toLowerCase() === searchQuery.toLowerCase() &&
+            Math.abs(city.lat - lat) < 0.001 &&
+            Math.abs(city.lon - lon) < 0.001);
+
+        console.log('foundWeather:', foundWeather);
+
+
+// **** Trying to understand why this original code is not working ****            
+            // let lat = req.query.lat;
+            // let lon = req.query.lon;
+            // let searchQuery = req.query.searchQuery;
+    
+            // let foundWeather = weatherData.find(city => city.city_name.toLowerCase() === searchQuery.toLowerCase() && city.lat == lat && city.lon == lon);
+
+        if (!foundWeather) {
+            res.status(404).send('No weather found');
+        }
+
+        let forecasts = foundWeather.data.map(weatherData => new Forecast(weatherData));
+
+        res.status(200).send(forecasts);
+    } catch (error) {
+        next(error);
+    }
+});
+
+// **** CLASS TO CLEAN UP BULKY DATA ****
+class Forecast {
+    constructor(forecastData) {
+        this.date = forecastData.datetime;
+        this.description = forecastData.weather.description;
+    }
+}
+
+// **** CATCH ALL SHOULD BE THE LAST DEFINED ****
+app.get('*', (req, res) => {
+    res.status(404).send('This page is not available');
+});
+
+app.use((error, req, res, next) => {
+    console.log(error.message);
+    res.status(500).send(error.message);
+});
